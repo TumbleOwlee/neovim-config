cf224740939e7026b4eb834e0f005772705334f6
David Loewe <49597367+TumbleOwlee@users.noreply.github.com>
2024-04-18 16:55:13 +0000

    Enable local config, support template replacements

--- init.lua
+++ init.lua
@@ -84,6 +84,10 @@ require('packer').startup(function()
     use { 'windwp/nvim-autopairs' } -- auto closing brackets
 end)
 
+--Enable local nvim files
+vim.o.exrc = true
+vim.o.secure = true
+
 --Incremental live completion
 vim.o.inccommand = 'nosplit'
 
@@ -230,6 +234,35 @@ if (is_module_available('indent_blankline')) then
         }
     }
 end
+
+-- Template placeholder replacement
+vim.api.nvim_create_autocmd({"BufRead"}, {
+    pattern = {"*"},
+    callback = function()
+        local buf_name = vim.api.nvim_buf_get_name(0)
+        local context = vim.g.template_context_map or {}
+
+        local config = {}
+        for pattern, cfg in pairs(context) do
+            if string.match(buf_name, pattern) then
+                config = cfg
+                break
+            end
+        end
+
+        local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+        for nr, line in ipairs(lines) do
+            for key, value in pairs(config) do
+                line = line:gsub(key, value)
+            end
+            vim.api.nvim_buf_set_lines(0, nr - 1, nr, true, {line})
+        end
+
+        if lines[1] then
+            vim.api.nvim_win_set_cursor(0, {1, lines[1]:len()})
+        end
+    end
+})
 
 -- Gitsigns
 if (is_module_available('gitsigns')) then
