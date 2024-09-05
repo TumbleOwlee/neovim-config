09dc22d5fae1b7402cd41b399be0b0bfec4c2c26
David Loewe <49597367+TumbleOwlee@users.noreply.github.com>
2024-09-05 17:37:31 +0200

    Add support of tab indentation, colorize indentation

--- lua/plugins/pl-indent-blankline.lua
+++ lua/plugins/pl-indent-blankline.lua
@@ -4,20 +4,45 @@ require 'loader'.load_plugin({
     config = function()
         vim.opt.list = true
         vim.opt.listchars:append("eol:↴")
+
+        -- highlight for indentation
+        local highlight = {
+            "RainbowRed",
+            "RainbowYellow",
+            "RainbowBlue",
+            "RainbowOrange",
+            "RainbowGreen",
+            "RainbowViolet",
+            "RainbowCyan",
+        }
+
+        -- create the highlight groups in the highlight setup hook, so they are reset
+        -- every time the colorscheme changes
+        require 'ibl.hooks'.register(require 'ibl.hooks'.type.HIGHLIGHT_SETUP, function()
+            vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+            vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+            vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+            vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+            vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+            vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+            vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+        end)
+
         require("ibl").setup {
             indent = {
                 char = '┊',
-                highlight = 'LineNr',
+                tab_char = '┊',
+                highlight = highlight,
             },
             scope = {
+                enabled = true,
                 show_end = true,
             },
             exclude = {
                 filetypes = { 'help', 'packer', 'dashboard' },
             },
-            --    buftype_exclude = { 'terminal', 'nofile' },
             whitespace = {
-                highlight = highlight,
+                highlight = { 'LineNr' },
                 remove_blankline_trail = true,
             }
         }
