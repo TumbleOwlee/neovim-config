4c8d237b3f9c18d8f8812e069d4b8e6d3dbfc943
David Loewe <49597367+TumbleOwlee@users.noreply.github.com>
2026-01-12 00:52:05 +0100

    Add markview for markdown rendering in nvim

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "OXY2DEV/markview.nvim",
+        opts = {
+            preview = {
+                icon_provider = "mini"
+            }
+        },
+        dependencies = {
+            "nvim-mini/mini.icons"
+        },
+        lazy = false
+    }
+}
