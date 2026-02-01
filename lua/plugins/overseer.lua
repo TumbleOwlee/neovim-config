0d38f40c7523a670758ac22a7407c45e730623ff
David Loewe <49597367+TumbleOwlee@users.noreply.github.com>
2026-02-01 15:02:15 +0100

    Add LSP progress notifier, add CMake command

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -1,183 +1,224 @@
 return {
-  {
-    "stevearc/resession.nvim",
-    lazy = true,
-    opts = {
-      extensions = {
-        overseer = {
-          filter = {
-            status = { "RUNNING" },
-          },
+    {
+        "stevearc/resession.nvim",
+        lazy = true,
+        opts = {
+            extensions = {
+                overseer = {
+                    filter = {
+                        status = { "RUNNING" },
+                    },
+                },
+            },
         },
-      },
     },
-  },
-  {
+    {
         'ibhagwan/fzf-lua',
         opts = {},
         config = function()
-            require'fzf-lua'.setup({})
-            require'fzf-lua'.register_ui_select()
+            require 'fzf-lua'.setup({})
+            require 'fzf-lua'.register_ui_select()
         end
-  },
-  {
-    "stevearc/overseer.nvim",
-    dependencies = {
-        'ibhagwan/fzf-lua',
     },
-    cmd = {
-      "Grep",
-      "Make",
-      "OverseerShell",
-      "OverseerOpen",
-      "OverseerRun",
-      "OverseerToggle",
-      "OverseerTestOutput",
-    },
-    keys = {
-      { "<leader>oo", "<cmd>OverseerToggle! bottom<CR>", mode = "n", desc = "[O]verseer [O]pen" },
-      { "<leader>or", "<cmd>OverseerRun<CR>", mode = "n", desc = "[O]verseer [R]un" },
-      { "<leader>os", "<cmd>OverseerShell<CR>", mode = "n", desc = "[O]verseer [S]hell" },
-      { "<leader>ot", "<cmd>OverseerTaskAction<CR>", mode = "n", desc = "[O]verseer [T]ask action" },
-      {
-        "<leader>od",
-        function()
-          local overseer = require("overseer")
-          local task_list = require("overseer.task_list")
-          local tasks = overseer.list_tasks({
-            sort = task_list.sort_finished_recently,
-            include_ephemeral = true,
-          })
-          if vim.tbl_isempty(tasks) then
-            vim.notify("No tasks found", vim.log.levels.WARN)
-          else
-            local most_recent = tasks[1]
-            overseer.run_action(most_recent)
-          end
-        end,
-        mode = "n",
-        desc = "[O]verseer [D]o quick action",
-      },
-    },
-    ---@module 'overseer'
-    ---@type overseer.SetupOpts
-    opts = {
-      dap = false,
-      log_level = vim.log.levels.TRACE,
-      component_aliases = {
-        default = {
-          "on_exit_set_status",
-          { "on_complete_notify", system = "unfocused" },
-          { "on_complete_dispose", require_view = { "SUCCESS", "FAILURE" } },
+    {
+        "stevearc/overseer.nvim",
+        dependencies = {
+            'ibhagwan/fzf-lua',
         },
-        default_neotest = {
-          "unique",
-          { "on_complete_notify", system = "unfocused", on_change = true },
-          "default",
+        cmd = {
+            "Grep",
+            "Make",
+            "CMake",
+            "OverseerOpen",
+            "OverseerRun",
+            "OverseerToggle",
+            "OverseerTestOutput",
         },
-      },
-      wrap_builtins = {
-        enabled = false,
-        partial_condition = {
-          noop = function(cmd, caller, opts) return true end,
-        },
-      },
-      post_setup = {},
-      task_list = {
-        direction = "float",
-      },
-      strategy = {
-        "terminal",
-        use_shell  = true,
-        direction = "float",
-        autoshow = "on_load",
-      },
-    },
-    init = function() vim.cmd.cnoreabbrev("OS OverseerShell") end,
-    config = function(_, opts)
-      local partial = opts.wrap_builtins.partial_condition
-      opts.wrap_builtins.condition = function(cmd, caller, opts)
-        for _, v in pairs(partial) do
-          if not v(cmd, caller, opts) then
-            return false
-          end
-        end
-        return true
-      end
-      local overseer = require("overseer")
-      overseer.setup(opts)
-      for _, cb in pairs(opts.post_setup) do
-        cb()
-      end
-      vim.api.nvim_create_user_command("OverseerTestOutput", function(params)
-        vim.cmd.tabnew()
-        vim.bo.bufhidden = "wipe"
-        overseer.create_task_output_view(0, {
-          select = function(self, tasks)
-            for _, task in ipairs(tasks) do
-              if task.metadata.neotest_group_id then
-                return task
-              end
-            end
-            self:dispose()
-          end,
-        })
-      end, {
-        desc = "Open a new tab that displays the output of the most recent test",
-      })
-      vim.api.nvim_create_user_command("Grep", function(params)
-        local args = vim.fn.expandcmd(params.args)
-        -- Insert args at the '$*' in the grepprg
-        local cmd, num_subs = vim.o.grepprg:gsub("%$%*", args)
-        if num_subs == 0 then
-          cmd = cmd .. " " .. args
-        end
-        local cwd
-        local has_oil, oil = pcall(require, "oil")
-        if has_oil then
-          cwd = oil.get_current_dir()
-        end
-
-        local task = overseer.new_task({
-          cmd = cmd,
-          cwd = cwd,
-          name = "grep " .. args,
-          components = {
+        keys = {
+            { "<leader>oo", "<cmd>OverseerRestartLast<CR>",    mode = "n", desc = "[O]verseer Restart [L]ast" },
+            { "<leader>oo", "<cmd>OverseerToggle! bottom<CR>", mode = "n", desc = "[O]verseer [O]pen" },
+            { "<leader>or", "<cmd>OverseerRun<CR>",            mode = "n", desc = "[O]verseer [R]un" },
+            { "<leader>os", "<cmd>OverseerShell<CR>",          mode = "n", desc = "[O]verseer [S]hell" },
+            { "<leader>ot", "<cmd>OverseerTaskAction<CR>",     mode = "n", desc = "[O]verseer [T]ask action" },
             {
-              "on_output_quickfix",
-              errorformat = vim.o.grepformat,
-              open = not params.bang,
-              open_height = 8,
-              items_only = true,
+                "<leader>od",
+                function()
+                    local overseer = require("overseer")
+                    local task_list = require("overseer.task_list")
+                    local tasks = overseer.list_tasks({
+                        sort = task_list.sort_finished_recently,
+                        include_ephemeral = true,
+                    })
+                    if vim.tbl_isempty(tasks) then
+                        vim.notify("No tasks found", vim.log.levels.WARN)
+                    else
+                        local most_recent = tasks[1]
+                        overseer.run_action(most_recent)
+                    end
+                end,
+                mode = "n",
+                desc = "[O]verseer [D]o quick action",
             },
-            -- We don't care to keep this around as long as most tasks
-            { "on_complete_dispose", timeout = 30, require_view = {} },
-            "default",
-          },
-        })
-        task:start()
-      end, { nargs = "*", bang = true, bar = true, complete = "file" })
+        },
+        ---@module 'overseer'
+        ---@type overseer.SetupOpts
+        opts = {
+            dap = false,
+            log_level = vim.log.levels.TRACE,
+            component_aliases = {
+                default = {
+                    "on_exit_set_status",
+                    { "on_complete_notify",  system = "unfocused" },
+                    { "on_complete_dispose", require_view = { "SUCCESS", "FAILURE" } },
+                },
+                default_neotest = {
+                    "unique",
+                    { "on_complete_notify", system = "unfocused", on_change = true },
+                    "default",
+                },
+            },
+            wrap_builtins = {
+                enabled = false,
+                partial_condition = {
+                    noop = function(cmd, caller, opts) return true end,
+                },
+            },
+            post_setup = {},
+            task_list = {
+                direction = "float",
+            },
+            strategy = {
+                "terminal",
+                use_shell = true,
+                direction = "float",
+                autoshow  = "on_load",
+            },
+        },
+        init = function() vim.cmd.cnoreabbrev("OS OverseerShell") end,
+        config = function(_, opts)
+            local partial = opts.wrap_builtins.partial_condition
+            opts.wrap_builtins.condition = function(cmd, caller, opts)
+                for _, v in pairs(partial) do
+                    if not v(cmd, caller, opts) then
+                        return false
+                    end
+                end
+                return true
+            end
+            local overseer = require("overseer")
+            overseer.setup(opts)
+            for _, cb in pairs(opts.post_setup) do
+                cb()
+            end
+            vim.api.nvim_create_user_command("OverseerTestOutput", function(params)
+                vim.cmd.tabnew()
+                vim.bo.bufhidden = "wipe"
+                overseer.create_task_output_view(0, {
+                    select = function(self, tasks)
+                        for _, task in ipairs(tasks) do
+                            if task.metadata.neotest_group_id then
+                                return task
+                            end
+                        end
+                        self:dispose()
+                    end,
+                })
+            end, {
+                desc = "Open a new tab that displays the output of the most recent test",
+            })
+            vim.api.nvim_create_user_command("Grep", function(params)
+                local args = vim.fn.expandcmd(params.args)
+                -- Insert args at the '$*' in the grepprg
+                local cmd, num_subs = vim.o.grepprg:gsub("%$%*", args)
+                if num_subs == 0 then
+                    cmd = cmd .. " " .. args
+                end
+                local cwd
+                local has_oil, oil = pcall(require, "oil")
+                if has_oil then
+                    cwd = oil.get_current_dir()
+                end
 
-      vim.api.nvim_create_user_command("Make", function(params)
-        -- Insert args at the '$*' in the makeprg
-        local cmd, num_subs = vim.o.makeprg:gsub("%$%*", params.args)
-        if num_subs == 0 then
-          cmd = cmd .. " " .. params.args
-        end
-        local task = require("overseer").new_task({
-          cmd = vim.fn.expandcmd(cmd),
-          components = {
-            { "on_output_quickfix", open = not params.bang, open_height = 8 },
-            "unique",
-            "default",
-          },
-        })
-        task:start()
-      end, {
-        desc = "Run your makeprg as an Overseer task",
-        nargs = "*",
-        bang = true,
-      })
-    end,
-  },
+                local task = overseer.new_task({
+                    cmd = cmd,
+                    cwd = cwd,
+                    name = "grep " .. args,
+                    components = {
+                        {
+                            "on_output_quickfix",
+                            errorformat = vim.o.grepformat,
+                            open = not params.bang,
+                            open_height = 8,
+                            items_only = true,
+                        },
+                        -- We don't care to keep this around as long as most tasks
+                        { "on_complete_dispose", timeout = 30, require_view = {} },
+                        "default",
+                    },
+                })
+                task:start()
+            end, { nargs = "*", bang = true, bar = true, complete = "file" })
+
+            vim.api.nvim_create_user_command("Make", function(params)
+                -- Insert args at the '$*' in the makeprg
+                local cmd, num_subs = vim.o.makeprg:gsub("%$%*", params.args)
+                if num_subs == 0 then
+                    cmd = cmd .. " " .. params.args
+                end
+                local task = require("overseer").new_task({
+                    cmd = vim.fn.expandcmd(cmd),
+                    components = {
+                        "unique",
+                        "default",
+                    },
+                })
+                task:start()
+                vim.notify("Start '" .. vim.fn.expandcmd(cmd) .. "'", vim.log.levels.INFO, { title = "Make" })
+            end, {
+                desc = "Run your makeprg as an Overseer task",
+                nargs = "*",
+                bang = true,
+            })
+
+            vim.api.nvim_create_user_command("OverseerRestartLast", function()
+                local overseer = require("overseer")
+                local task_list = require("overseer.task_list")
+                local tasks = overseer.list_tasks({
+                    status = {
+                        overseer.STATUS.SUCCESS,
+                        overseer.STATUS.FAILURE,
+                        overseer.STATUS.CANCELED,
+                    },
+                    sort = task_list.sort_finished_recently
+                })
+                if vim.tbl_isempty(tasks) then
+                    vim.notify("No tasks found", vim.log.levels.WARN)
+                else
+                    local most_recent = tasks[1]
+                    overseer.run_action(most_recent, "restart")
+                end
+            end, {})
+
+            vim.api.nvim_create_user_command("CMake", function(params)
+                -- Insert args at the '$*' in the makeprg
+                local cmd = "cmake"
+                if params.args:len() > 0 then
+                    cmd = cmd .. " " .. params.args
+                end
+                local task = require("overseer").new_task({
+                    cmd = vim.fn.expandcmd(cmd),
+                    components = {
+                        "unique",
+                        "default",
+                    },
+                })
+                task:start()
+                vim.notify("Start '" .. vim.fn.expandcmd(cmd) .. "'", vim.log.levels.INFO, { title = "CMake" })
+            end, {
+                desc = "Run your makeprg as an Overseer task",
+                nargs = "*",
+                bang = true,
+            })
+        end,
+    },
 }
